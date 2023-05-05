681efa004f69da03775e9942a6c86a2a97c3a73c
Victor Lima <victoraamlima@gmail.com>
2023-05-05 14:39:24 -0300

    index.js refactoring

--- js/index.js
+++ js/index.js
@@ -1,78 +1,28 @@
-let submitSelected = document.querySelector("button[id = 'button']")
-
-// console.log(submitSelected.classList)
-
-
-let scores = document.querySelectorAll('input[name="score"]')
-
-// console.log(document.querySelector('button[id="button"]'))
-
-
-let scoreSelected = document.getElementById("your-score-selected")
-
-let ops = document.getElementsByClassName("ops")
-
-
-
-let card1 = document.getElementsByClassName("card1")
-let card2 = document.getElementsByClassName("card2")
+const submitSelected = document.querySelector("button[id = 'button']")
+const scores = document.querySelectorAll('input[name="score"]')
+const scoreSelected = document.getElementById("your-selected-score")
+const rating = document.getElementsByClassName("rating")
+const thank = document.getElementsByClassName("thank")
 
 function goToThankYou() {
-    card1[0].classList.add("hide")
-    card2[0].classList.remove("hide")
+  rating[0].classList.add("hide")
+  thank[0].classList.remove("hide")
 }
 
 function checkedRadio() {
-    let radioSelected = document.querySelector("input[name='score']:checked").id
+  let radioSelected = document.querySelector("input[name='score']:checked").id
 
-    scoreSelected.textContent = `AMOR, você escolheu ${radioSelected} de 5!`
+  scoreSelected.textContent = `You select ${radioSelected} de 5!`
 }
 
-
-// for (let i = 0; i < scores.length; i++) {
-
-//     scores[i].addEventListener("change", function(){
-
-
-//         submitSelected.classList.add("hover")
-//         submitSelected.classList.remove("inactive")
-
-//     })
-
-for (let i = 0; i < 5; i++) {
-
-    scores[i].addEventListener("change", function(){
-
-
-        ops[0].classList.remove("hide")
-
-        if (scores[4].checked) {
-            ops[0].classList.add("hide")
-        }
-    })
-
-    
-}
-
-scores[4].addEventListener("change", function () {
+for (let i = 0; i < scores.length; i++) {
+  scores[i].addEventListener("change", function () {
     submitSelected.classList.add("hover")
     submitSelected.classList.remove("inactive")
-})
-
-
-
+  })
+}
 
 submitSelected.addEventListener("click", function () {
-    // console.log(submitSelected)
-
-    if (scores[4].checked) {
-        // console.log(scores[4].checked)
-        goToThankYou()
-
-        checkedRadio()
-    }
-
-    
-
-    
+  goToThankYou()
+  checkedRadio()
 })
